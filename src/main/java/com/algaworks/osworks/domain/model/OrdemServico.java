4bdd75e0877a98f928d8e5fd8e3ff67f490ca437
Renanvt <jonatan_rvs@hotmail.com>
2020-10-23 08:58:56 -0300

    Criando as classes e migrations

---
+++ src/main/java/com/algaworks/osworks/domain/model/OrdemServico.java
@@ -0,0 +1,97 @@
+package com.algaworks.osworks.domain.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class OrdemServico {
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private long id;
+	
+	//Não é uma associação de uma colunar qualquer, e sim de um relacionamento 
+	//de muitos para um
+	//@JoinColumn(name = "cliente_id")
+	@ManyToOne 
+	private Cliente cliente;
+	
+	private String descricao;
+	private BigDecimal peso;
+	
+	@Enumerated(EnumType.STRING) //Na coluna de status, armazena a string ABERTA, FINALIZADA, etc, e nao numero
+	private StatusOrdemServico status;
+	private LocalDateTime dataAbertura;
+	private LocalDateTime dataFinalizacao;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public BigDecimal getPeso() {
+		return peso;
+	}
+	public void setPeso(BigDecimal peso) {
+		this.peso = peso;
+	}
+	public StatusOrdemServico getStatus() {
+		return status;
+	}
+	public void setStatus(StatusOrdemServico status) {
+		this.status = status;
+	}
+	public LocalDateTime getDataAbertura() {
+		return dataAbertura;
+	}
+	public void setDataAbertura(LocalDateTime dataAbertura) {
+		this.dataAbertura = dataAbertura;
+	}
+	public LocalDateTime getDataFinalizacao() {
+		return dataFinalizacao;
+	}
+	public void setDataFinalizacao(LocalDateTime dataFinalizacao) {
+		this.dataFinalizacao = dataFinalizacao;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrdemServico other = (OrdemServico) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+	
+}
