c251598735d1b6e16008268a7f43402e2abf88c4
Renanvt <jonatan_rvs@hotmail.com>
2020-10-24 20:42:21 -0300

    Ajustando o codigo

--- src/main/java/com/algaworks/osworks/api/controller/OrdemServicoController.java
+++ src/main/java/com/algaworks/osworks/api/controller/OrdemServicoController.java
@@ -2,6 +2,7 @@ package com.algaworks.osworks.api.controller;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
@@ -36,22 +37,33 @@ public class OrdemServicoController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public OrdemServico criar(@Valid @RequestBody OrdemServico ordemServico) {
-		return gestaoOrdemServicoService.criar(ordemServico);
+	public OrdemServicoModel criar(@Valid @RequestBody OrdemServico ordemServico) {
+		return toModel(gestaoOrdemServicoService.criar(ordemServico));
 	}
 	@GetMapping
-	public List<OrdemServico> listar(){
-		return ordemServicoRepository.findAll();
+	public List<OrdemServicoModel> listar(){
+		return toCollectionModel(ordemServicoRepository.findAll());
 	}
 	
 	@GetMapping("/{ordemServicoId}")
 	public ResponseEntity<OrdemServicoModel> buscar(@PathVariable Long ordemServicoId){
 		Optional<OrdemServico> ordemServico = ordemServicoRepository.findById(ordemServicoId);
 		if(ordemServico.isPresent()) { //Se não é nulo
-			OrdemServicoModel ordemServicoModel = modelMapper.map(ordemServico.get(), OrdemServicoModel.class);
+			OrdemServicoModel ordemServicoModel = toModel(ordemServico.get());
 			return ResponseEntity.ok(ordemServicoModel);
 		}
 		
 		return ResponseEntity.notFound().build();
+	}
+	
+	private OrdemServicoModel toModel(OrdemServico ordemServico) {
+		return modelMapper.map(ordemServico, OrdemServicoModel.class);
+		
+	}
+	private List<OrdemServicoModel> toCollectionModel(List<OrdemServico> ordensServico){
+		return ordensServico.stream().
+				map(ordemServico -> toModel(ordemServico))
+				.collect(Collectors.toList());
+				
 	}
 }
